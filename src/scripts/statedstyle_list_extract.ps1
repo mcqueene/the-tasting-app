fe53ee801da05d5feb159aa39d5ce9956b74de87
matt <mcqueene@hotmail.com>
2024-03-03 13:31:39 -0600

    mar 3 udpate

--- src/scripts/statedstyle_list_extract.ps1
+++ src/scripts/statedstyle_list_extract.ps1
@@ -17,6 +17,11 @@ $brewerarray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other
 
 [array]$errorlist = @()
 $grouparray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other Beverage*' | Group-Object StatedStyle  | Sort-Object Name | Where-Object Count -lt 7 
+
+[array]$toplist = @()
+$toparray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other Beverage*' | Group-Object StatedStyle | Where-Object Count -gt 100  | Sort-Object Name 
+
+
 foreach($row in $grouparray) {
     [string]$style = $row.Name
     [string]$count = $row.Count
@@ -33,15 +38,34 @@ foreach($row in $grouparray) {
     }
 }
 
+foreach($row in $toparray) {
+    [string]$style = $row.Name
+    $top5array = $row.Group | Sort-Object OverallScore, DateTasted -Descending | Select-Object -First 5
+    foreach($g in $top5array) {
+        $obj = new-object PSObject
+        $obj | add-member -membertype NoteProperty -name "Style" -Value $style
+        $obj | add-member -membertype NoteProperty -name "Beer" -Value $g.Beer
+        $obj | add-member -membertype NoteProperty -name "Brewer" -Value $g.Brewer
+        $obj | add-member -membertype NoteProperty -name "StateCountry" -Value $g.StateCountry
+        $obj | add-member -membertype NoteProperty -name "ABV" -Value $g.ABV
+        $obj | add-member -membertype NoteProperty -name "DateTasted" -Value $g.DateTasted
+        $obj | add-member -membertype NoteProperty -name "OverallScore" -Value $g.OverallScore
+        $toplist += $obj    
+    }
+}
+
 Write-Host 'removing old files'
 Remove-Item 'C:\Users\matt\OneDrive\Beer Club\StatedStyle.xlsx'
+Remove-Item 'C:\Users\matt\OneDrive\Beer Club\top5list.xlsx' 
 $excelpkg1 = $array | Export-Excel -PassThru -AutoSize -WorksheetName 'Styles' -Path 'C:\Users\matt\OneDrive\Beer Club\StatedStyle.xlsx' -TableName 'Styles' -TableStyle Medium16 
 Set-ExcelColumn -ExcelPackage $excelpkg1 -WorksheetName "Styles" -Column 1 -Width 30
 $sourcearray | Group-Object StatedStyle | Sort-Object Name | Select-Object Name, Count | Export-Excel -PassThru -AutoSize -WorksheetName 'StyleCount' -ExcelPackage $excelpkg1 -TableName 'StyleCount' -TableStyle Medium16 
 Set-ExcelColumn -ExcelPackage $excelpkg1 -WorksheetName "StyleCount" -Column 1 -Width 30
 $errorlist | Sort-Object Count, Style | Export-Excel -PassThru -AutoSize -WorksheetName 'LowCountStyles' -ExcelPackage $excelpkg1 -TableName 'LowCountStyles' -TableStyle Medium16 
-$brewerarray | Select-Object Name, Count | Export-Excel -PassThru -AutoSize -WorksheetName 'BrewerCount' -ExcelPackage $excelpkg1 -TableName 'BrewerCount' -TableStyle Medium16 
+$brewerarray | Select-Object Name, Count | Sort-Object Name | Export-Excel -PassThru -AutoSize -WorksheetName 'BrewerCount' -ExcelPackage $excelpkg1 -TableName 'BrewerCount' -TableStyle Medium16 
+$excelpkg2 = $toplist | Export-Excel -PassThru -AutoSize -WorksheetName 'TopList' -Path 'C:\Users\matt\OneDrive\Beer Club\top5list.xlsx' -TableName 'TopList' -TableStyle Medium16 
 Close-ExcelPackage -ExcelPackage $excelpkg1 -Show
+Close-ExcelPackage -ExcelPackage $excelpkg2 -Show
 
 #Remove-Item 'C:\Users\matt\OneDrive\Beer Club\StatedStyle_Count.xlsx'
 #$sourcearray | Group-Object StatedStyle | Sort-Object Name | Select-Object Name, Count | Export-Excel -Path 'C:\Users\matt\OneDrive\Beer Club\StatedStyle_Count.xlsx' -TableName 'StyleCount' -TableStyle Medium16 -Show
