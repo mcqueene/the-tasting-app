7c64b8ded8386557edce8792cedd192a7d02206d
matt <mcqueene@hotmail.com>
2024-05-19 20:12:19 -0500

    add avg score report

--- src/scripts/statedstyle_list_extract.ps1
+++ src/scripts/statedstyle_list_extract.ps1
@@ -17,11 +17,6 @@ $brewerarray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other
 
 [array]$errorlist = @()
 $grouparray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other Beverage*' | Group-Object StatedStyle  | Sort-Object Name | Where-Object Count -lt 7 
-
-[array]$toplist = @()
-$toparray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other Beverage*' | Group-Object StatedStyle | Where-Object Count -gt 100  | Sort-Object Name 
-
-
 foreach($row in $grouparray) {
     [string]$style = $row.Name
     [string]$count = $row.Count
@@ -38,6 +33,8 @@ foreach($row in $grouparray) {
     }
 }
 
+[array]$toplist = @()
+$toparray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other Beverage*' | Group-Object StatedStyle | Where-Object Count -gt 100  | Sort-Object Name 
 foreach($row in $toparray) {
     [string]$style = $row.Name
     $top5array = $row.Group | Sort-Object OverallScore, DateTasted -Descending | Select-Object -First 5
@@ -54,6 +51,37 @@ foreach($row in $toparray) {
     }
 }
 
+
+[array]$avgscorelist = @()
+$avgscorearray = $sourcearray | Where-Object -Property StatedStyle -NotLike 'Other Beverage*' | Group-Object DateTasted
+foreach($row in $avgscorearray) {
+    [string]$datetasted = $row.Name
+    [string]$beerlist = ''
+    [int]$count = 0
+    [decimal]$abv = 0
+    [decimal]$avgscore = 0
+    foreach($g in $row.Group) {
+        $count +=1
+        $avgscore += $g.OverallScore
+        $abv += $g.ABV
+        [string]$temp = $g.Beer
+        $beerlist += ($temp + ';')
+        #Write-Host $g
+    }
+    $obj = new-object PSObject
+    $obj | add-member -membertype NoteProperty -name "DateTasted" -Value $datetasted
+    $obj | add-member -membertype NoteProperty -name "Count" -Value $count
+    $obj | add-member -membertype NoteProperty -name "AvgScore" -Value ($avgscore / $count)
+    $obj | add-member -membertype NoteProperty -name "ABV" -Value ($abv / $count)
+    $obj | add-member -membertype NoteProperty -name "BeerList" -Value $beerlist
+    $avgscorelist += $obj    
+}
+Remove-Item 'C:\Users\matt\OneDrive\Beer Club\avgscorelist.xlsx' 
+$excelpkg3 = $avgscorelist | Export-Excel -PassThru -AutoSize -WorksheetName 'AvgScores' -Path 'C:\Users\matt\OneDrive\Beer Club\avgscorelist.xlsx' -TableName 'AvgScores' -TableStyle Medium16 
+Set-ExcelColumn -ExcelPackage $excelpkg3 -WorksheetName "AvgScores" -Column 5 -Width 150
+Close-ExcelPackage -ExcelPackage $excelpkg3 -Show
+
+
 Write-Host 'removing old files'
 Remove-Item 'C:\Users\matt\OneDrive\Beer Club\StatedStyle.xlsx'
 Remove-Item 'C:\Users\matt\OneDrive\Beer Club\top5list.xlsx' 
@@ -64,6 +92,8 @@ Set-ExcelColumn -ExcelPackage $excelpkg1 -WorksheetName "StyleCount" -Column 1 -
 $errorlist | Sort-Object Count, Style | Export-Excel -PassThru -AutoSize -WorksheetName 'LowCountStyles' -ExcelPackage $excelpkg1 -TableName 'LowCountStyles' -TableStyle Medium16 
 $brewerarray | Select-Object Name, Count | Sort-Object Name | Export-Excel -PassThru -AutoSize -WorksheetName 'BrewerCount' -ExcelPackage $excelpkg1 -TableName 'BrewerCount' -TableStyle Medium16 
 $excelpkg2 = $toplist | Export-Excel -PassThru -AutoSize -WorksheetName 'TopList' -Path 'C:\Users\matt\OneDrive\Beer Club\top5list.xlsx' -TableName 'TopList' -TableStyle Medium16 
+
+
 Close-ExcelPackage -ExcelPackage $excelpkg1 -Show
 Close-ExcelPackage -ExcelPackage $excelpkg2 -Show
 
